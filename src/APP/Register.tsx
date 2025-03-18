3994a0afdb05711ab7d8f28435e00ab11fef43ad
“gave” <adeun2020@gmail.com>
2025-03-18 13:08:05 -0700

    e

--- src/APP/Register.tsx
+++ src/APP/Register.tsx
@@ -1,27 +1,120 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useSession} from "../Hook/useSession";
 import Input from "../component/UI/Input";
 import Button from "../component/UI/Button";
 import axios, {AxiosError} from "axios";
 import Toast from "../component/Toast";
 import { useNavigate,} from 'react-router-dom';
+import z from "zod";
 
+type Meg ={
+    message: string,
+    show: boolean,
+    type:"danger"|"success"|"warning"|"default"
 
+}
 
 function Register() {
     const navigate = useNavigate();
     useSession();
     const [signStart , setSignStart] = useState<boolean>(false);
-    const [error, setError] = useState("");
+    const [message, setMessage] = useState<Meg>({
+        message: "",
+        type:"default",
+        show: false
+    });
     const [loginInfo, setLoginInfo] = useState({
         username: "",
         password: "",
         email: "",
-        password2: "",
     })
+    const Z_loginInfo = z.object({
+        username: z.string().min(3, 'Username must be at least 3 characters long').max(50, 'Username must be under 50 characters'),
+        email: z.string().email('Invalid email address'),
+        password: z.string().min(8, 'New password must be at least 8 characters long'),
+
+
+    })
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setMessage({
+                message:"",
+                show:false,
+                type:"default"
+            })
+        },2000)
+
+        return () => {clearTimeout(timer)}
+
+    },[message.show])
+
+   async function handleSubmit() {
+        const {data ,error} = Z_loginInfo.safeParse(loginInfo)
+        setSignStart(true);
+        if (error) {
+            setMessage({
+                message:error.errors[0].message,
+                type:"danger",
+                show: true
+            })
+            setSignStart(false);
+            return;
+
+        }
+
+        if (data){
+            try {
+                await axios.post("https://nodevap.onrender.com/api/Register", {
+                    username: loginInfo.username.trim(),
+                    password: loginInfo.password.trim(),
+                    email: loginInfo.email.trim(),
+
+
+                })
+
+
+                setLoginInfo({
+                    username: "",
+                    password: "",
+                    email: "",
+
+                })
+                setSignStart(false);
+                navigate("/login");
+
+            }catch(err){
+                const {response} = err as AxiosError<{error:string |null ,data:object |null }>;
+                if (response && response.data.error ){
+                    setMessage({
+                        message:response.data.error,
+                        type:"danger",
+                        show: true
+                    })
+
+
+                    console.log(response.data.error)
+
+                }else {
+
+
+                    setMessage({
+                        message:"something went wrong",
+                        type:"danger",
+                        show: true
+                    })
+
+                }
+            }finally {
+                setSignStart(false);
+            }
+
+
+        }
+
+    }
     return (
         <div className={"ROOTX Register"}>
-            {error && <Toast type={"danger"} value={error} />}
+            {message.show && <Toast type={message.type} value={message.message} />}
             <Input
                 LabelI={"Username"}
                 showLabel={true}
@@ -60,52 +153,10 @@ function Register() {
 
 
             />
-            <Input
-                isPassword={true}
-                LabelI={"Password"}
-                showLabel={true}
-                type='password'
-                placeholder='Password'
-                value={loginInfo.password2}
-                disabled={signStart}
-                className={"w-55"}
-                onChange={(e)=>{setLoginInfo(pre=>({...pre , password2: e.target.value}))}}
 
-
-            />
             <Button
                 className="loginButton"
-                onClick={ async () => {
-                    if (loginInfo.username && loginInfo.password && loginInfo.password2  && loginInfo.email && loginInfo.password2) {
-                        console.log(loginInfo)
-                        setSignStart(true)
-                         await axios.post("https://nodevap.onrender.com/api/Register", {
-                             username: loginInfo.username.trim(),
-                             password: loginInfo.password.trim(),
-                             email: loginInfo.email.trim(),
-
-
-                         }).then((data)=>{
-                            console.log(data);
-                            navigate("/login");
-                        }).catch((err)=>{
-                             const {response} = err as AxiosError<{ error: string |null, data: object |null}>
-                             console.log(response?.data.error);
-                             setError(response?.data.error||"");
-                        })
-
-
-
-                        setLoginInfo({
-                            username: "",
-                            password: "",
-                            email: "",
-                            password2: "",
-                        })
-                        setSignStart(false)
-
-                    }
-                }}
+                onClick={handleSubmit}
             > Register</Button>
 
         </div>
